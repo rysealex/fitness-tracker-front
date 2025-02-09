b2ddc9ad38e8a413ed39b8009b9ebfb1f4779ee3
Alex Ryse <aryse54@gmail.com>
2025-02-09 02:52:08 -0800

    Creates the main navigation bar including GUI improvements and functionality.

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -10,25 +10,86 @@ function Welcome() {
     navigate(url);
   };
   return (
-    <div className='welcome-container'>
-      <h1>Welcome</h1>
-      <h2>Fitness Tracker</h2>
-      <Stack className='button-container' spacing={2} direction="row" >
-        <Button 
-          className='log-in' 
-          variant="contained" 
-          onClick={() => handleNavigate("/login")}
-        >
-          Login
-        </Button>
-        <Button 
-          className='create-account' 
-          variant="contained" 
-          onClick={() => handleNavigate("/create-account")}
-        >
-          Create Account
-        </Button>
-      </Stack>
+    <div className='main-container'>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      <aside className='nav-container'>
+        <div className='nav-header'>
+          <img src='/images/muscle-logo.png' alt='logo'></img>
+          <h2>FitnessTracker</h2>
+        </div>
+        <ul className='nav-links'>
+          <h4>
+            <span>Main Menu</span>
+            <div className='menu-separator'></div>
+          </h4>
+          <li>
+            <a href='#'><span 
+            class="material-symbols-outlined">
+              dashboard
+              </span>Dashboard</a>
+          </li>
+          <li>
+            <a href='#'><span 
+            class="material-symbols-outlined">
+              monitoring
+              </span>Stats</a>
+          </li>
+          <li>
+            <a href='#'><span 
+            class="material-symbols-outlined">
+              notifications_active
+              </span>Notifications</a>
+          </li>
+          <li>
+            <a href='#'><span 
+            class="material-symbols-outlined">
+              account_circle
+              </span>Profile</a>
+          </li>
+          <li>
+            <a href='#'><span 
+            class="material-symbols-outlined">
+              settings
+              </span>Settings</a>
+          </li>
+          <li>
+            <a href='#'><span 
+            class="material-symbols-outlined">
+              logout
+              </span>Logout</a>
+          </li>
+        </ul>
+        <div className='user-account'>
+          <div className='user-profile'>
+            <img src='/images/yami.png'
+            alt='profile-img'></img>
+            <div className='user-detail'>
+              <h3>Alex Ryse</h3>
+              <span>Software Engineer</span>
+            </div>
+          </div>
+        </div>
+      </aside>
+      <div className='welcome-container'>
+        <h1>Welcome</h1>
+        <h2>Fitness Tracker</h2>
+        <Stack className='button-container' spacing={2} direction="row" >
+          <Button 
+            className='log-in' 
+            variant="contained" 
+            onClick={() => handleNavigate("/login")}
+          >
+            Login
+          </Button>
+          <Button 
+            className='create-account' 
+            variant="contained" 
+            onClick={() => handleNavigate("/create-account")}
+          >
+            Create Account
+          </Button>
+        </Stack>
+      </div>
     </div>
   );
 };
